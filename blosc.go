2237d548301d3087f988d97fe855ff50142e8b81
Daniel Wilson <dwilson@seerai.space>
2025-12-26 11:43:46 -0700

    refactor to use thread safe calls

--- blosc.go
+++ blosc.go
@@ -2,80 +2,223 @@
 package blosc
 
 /*
-#cgo CFLAGS: -O3 -msse2 -D HAVE_LZ4=1
 #cgo LDFLAGS: -lpthread -lblosc
 #include "blosc.h"
-int blosc_set_compressor_wrapper(char* compname) {
-	return blosc_set_compressor(compname);
-}
 */
 import "C"
 import (
-	"fmt"
-	"reflect"
-	"unsafe"
-
 	"errors"
+	"fmt"
+	"runtime"
+	"slices"
+	"strings"
+	"unsafe"
 )
 
-func init() {
-	C.blosc_init()
+type ShuffleType uint
+
+const (
+	NoShuffle         ShuffleType = C.BLOSC_NOSHUFFLE
+	ByteShuffle       ShuffleType = C.BLOSC_SHUFFLE
+	BitShuffle        ShuffleType = C.BLOSC_BITSHUFFLE
+	BitShuffleInvalid ShuffleType = 100
+	// Zarr Version 3 shuffle names
+	NoShuffleStr   = "noshuffle"
+	ByteShuffleStr = "shuffle"
+	BitShuffleStr  = "bitshuffle"
+	InvalidStr     = "invalid"
+)
+
+type CNameType string
+
+const (
+	BloscLZ CNameType = "blosclz"
+	LZ4     CNameType = "lz4"
+	LZ4HC   CNameType = "lz4hc"
+	SNAPPY  CNameType = "snappy"
+	ZLIB    CNameType = "zlib"
+	ZSTD    CNameType = "zstd"
+)
+
+func ValidCompressors() []CNameType {
+	return []CNameType{
+		BloscLZ,
+		LZ4,
+		LZ4HC,
+		SNAPPY,
+		ZLIB,
+		ZSTD,
+	}
 }
 
-// SetCompressor sets the compressor that blosc will use
-func SetCompressor(name string) error {
-	switch name {
-	case "blosclz", "lz4", "lz4hc":
-		break
+func ValidShuffles() []ShuffleType {
+	return []ShuffleType{
+		NoShuffle,
+		ByteShuffle,
+		BitShuffle,
+	}
+}
+
+func (s ShuffleType) String() string {
+	switch s {
+	case NoShuffle:
+		return NoShuffleStr
+	case ByteShuffle:
+		return ByteShuffleStr
+	case BitShuffle:
+		return BitShuffleStr
 	default:
-		return errors.New("only lz4, blosclz, lz4hc are currently supported")
+		return InvalidStr
 	}
-	str := C.CString(name)
-	defer C.free(unsafe.Pointer(str))
-
-	res := C.blosc_set_compressor_wrapper(str)
-	if res < 0 {
-		return fmt.Errorf("invalid compressor '%s'", name)
-	}
-	return nil
 }
 
-// SetBlocksize for the compressor
-func SetBlocksize(blocksize int) {
-	C.blosc_set_blocksize(C.ulong(blocksize))
+func (s *ShuffleType) FromString(str string) {
+	str = strings.ToLower(str)
+	switch str {
+	case "noshuffle":
+		*s = NoShuffle
+	case "shuffle":
+		*s = ByteShuffle
+	case "bitshuffle":
+		*s = BitShuffle
+	default:
+		*s = BitShuffleInvalid
+	}
+}
+
+var nThreads = runtime.NumCPU()
+
+type Context struct {
+	CName     CNameType
+	CLevel    uint
+	Shuffle   ShuffleType
+	BlockSize uint
+	TypeSize  uint
+	nThreads  uint
+}
+
+type ContextOption func(*Context) error
+
+// NewContext creates a new blosc context with the given parameters.
+func WithCompressor(name CNameType) ContextOption {
+	return func(c *Context) error {
+		if !slices.Contains(ValidCompressors(), name) {
+			validCompressors := make([]string, len(ValidCompressors()))
+			for i, v := range ValidCompressors() {
+				validCompressors[i] = string(v)
+			}
+			return fmt.Errorf("invalid compressor name, valid names are: %s", strings.Join(validCompressors, ", "))
+		}
+		c.CName = name
+
+		return nil
+	}
+}
+
+func WithCompressionLevel(level uint) ContextOption {
+	return func(c *Context) error {
+		c.CLevel = level
+		return nil
+	}
+}
+
+func WithShuffle(shuffle ShuffleType) ContextOption {
+	return func(c *Context) error {
+		if shuffle != NoShuffle && shuffle != ByteShuffle && shuffle != BitShuffle {
+			return errors.New("invalid shuffle type")
+		}
+		c.Shuffle = shuffle
+		return nil
+	}
+}
+
+func WithShuffleString(shuffle string) ContextOption {
+	return func(c *Context) error {
+		var s ShuffleType
+		s.FromString(shuffle)
+		if s > BitShuffle {
+			return fmt.Errorf("invalid shuffle type string, must be '%s', '%s', or '%s'", NoShuffleStr, ByteShuffleStr, BitShuffleStr)
+		}
+		c.Shuffle = s
+		return nil
+	}
+}
+
+func WithBlockSize(blocksize uint) ContextOption {
+	return func(c *Context) error {
+		c.BlockSize = blocksize
+		return nil
+	}
+}
+
+func WithTypeSize(typesize uint) ContextOption {
+	return func(c *Context) error {
+		c.TypeSize = typesize
+		return nil
+	}
+}
+
+func WithNThreads(threads uint) ContextOption {
+	return func(c *Context) error {
+		c.nThreads = threads
+		return nil
+	}
+}
+
+func NewContext(opts ...ContextOption) (*Context, error) {
+	c := &Context{
+		CName:     LZ4,
+		CLevel:    5,
+		Shuffle:   NoShuffle,
+		BlockSize: 0,
+		TypeSize:  1,
+		nThreads:  uint(nThreads),
+	}
+	for _, opt := range opts {
+		if err := opt(c); err != nil {
+			return nil, err
+		}
+	}
+
+	return c, nil
 }
 
 // Compress takes a slice of numbers and compresses according to level and shuffle.
-func Compress(level int, shuffle bool, slice interface{}) ([]byte, error) {
-
-	rv := reflect.ValueOf(slice)
-	if rv.Kind() != reflect.Slice {
-		return nil, errors.New("input data must be a slice")
-	}
-	l := rv.Len()
-	if l == 0 {
+func (c *Context) Compress(slice []byte) ([]byte, error) {
+	if len(slice) == 0 {
 		return []byte{}, nil
 	}
 
-	size := int(rv.Index(0).Type().Size())
-	ptr := unsafe.Pointer(rv.Pointer())
-
-	s := 1
-	if !shuffle {
-		s = 0
+	sliceLen := len(slice)
+	size := 1
+	if c.TypeSize > 0 {
+		size = int(c.TypeSize)
 	}
-	compressed := make([]byte, l*size+C.BLOSC_MAX_OVERHEAD)
-	csize := C.blosc_compress(C.int(level), C.int(s), C.size_t(size),
-		C.size_t(l*size),
+	ptr := unsafe.Pointer(&slice[0])
+
+	level := c.CLevel
+
+	compressed := make([]byte, sliceLen*size+C.BLOSC_MAX_OVERHEAD)
+
+	// c str for cname
+	cname := C.CString(string(c.CName))
+	defer C.free(unsafe.Pointer(cname))
+
+	csize := C.blosc_compress_ctx(C.int(level), C.int(c.Shuffle), C.size_t(size),
+		C.size_t(sliceLen),
 		ptr,
 		unsafe.Pointer(&compressed[0]),
-		C.size_t(len(compressed)))
+		C.size_t(len(compressed)),
+		cname,
+		C.size_t(c.BlockSize),
+		C.int(nThreads),
+	)
 
 	return compressed[:csize], nil
 }
 
 // Decompress takes a byte of compressed data and returns the uncompressed data.
-func Decompress(compressed []byte) []byte {
+func (c *Context) Decompress(compressed []byte) []byte {
 	if len(compressed) == 0 {
 		return []byte{}
 	}
@@ -87,6 +230,6 @@ func Decompress(compressed []byte) []byte {
 	C.blosc_cbuffer_sizes(unsafe.Pointer(&compressed[0]), &nbytes, &cbytes, &blksz)
 
 	data := make([]byte, int(nbytes))
-	C.blosc_decompress(unsafe.Pointer(&compressed[0]), unsafe.Pointer(&data[0]), nbytes)
+	C.blosc_decompress_ctx(unsafe.Pointer(&compressed[0]), unsafe.Pointer(&data[0]), nbytes, C.int(nThreads))
 	return data
 }
--- blosc_test.go
+++ blosc_test.go
@@ -1,8 +1,9 @@
 package blosc_test
 
 import (
+	"bytes"
+	"encoding/binary"
 	"testing"
-	"unsafe"
 
 	blosc "github.com/seerai/go-blosc"
 	"github.com/stretchr/testify/require"
@@ -10,33 +11,128 @@ import (
 
 func TestRoundTrip(t *testing.T) {
 
-	buf := make([]int64, 10000)
-	for i := 0; i < len(buf); i++ {
-		buf[i] = int64(112)
+	ints := make([]int64, 10000)
+	for i := 0; i < len(ints); i++ {
+		ints[i] = int64(112)
 	}
 
-	err := blosc.SetCompressor("lz4")
+	// convert to byte slice
+	buf := &bytes.Buffer{}
+	err := binary.Write(buf, binary.LittleEndian, ints)
 	require.NoError(t, err)
 
-	cmp, err := blosc.Compress(1, true, buf)
+	c, err := blosc.NewContext(
+		blosc.WithCompressor("lz4"),
+		blosc.WithCompressionLevel(9),
+		blosc.WithShuffleString("bitshuffle"),
+		blosc.WithShuffle(blosc.BitShuffle),
+		blosc.WithBlockSize(100),
+		blosc.WithTypeSize(8),
+		blosc.WithNThreads(2),
+	)
 	require.NoError(t, err)
-	dec := blosc.Decompress(cmp)
-	require.Equal(t, len(buf)*int(unsafe.Sizeof(buf[0])), len(dec))
 
-	obuf := (*(*[1 << 32]int64)(unsafe.Pointer(&dec[0])))[:len(dec)/int(unsafe.Sizeof(buf[0]))]
-	for i, o := range obuf {
-		require.Equal(t, buf[i], o)
+	cmp, err := c.Compress(buf.Bytes())
+	require.NoError(t, err)
+	dec := c.Decompress(cmp)
+	require.Equal(t, len(buf.Bytes()), len(dec))
+
+	for _, cName := range blosc.ValidCompressors() {
+		c, err := blosc.NewContext(
+			blosc.WithCompressor(cName),
+		)
+		require.NoError(t, err)
+
+		cmp, err := c.Compress(buf.Bytes())
+		require.NoError(t, err)
+		dec := c.Decompress(cmp)
+		require.Equal(t, len(buf.Bytes()), len(dec))
 	}
 
+	// for each shuffle type
+	for _, shuffle := range blosc.ValidShuffles() {
+		c, err := blosc.NewContext(
+			blosc.WithShuffle(shuffle),
+		)
+		require.NoError(t, err)
+
+		cmp, err := c.Compress(buf.Bytes())
+		require.NoError(t, err)
+		dec := c.Decompress(cmp)
+		require.Equal(t, len(buf.Bytes()), len(dec))
+	}
+
+	// Invalid shuffle string
+	_, err = blosc.NewContext(
+		blosc.WithShuffleString("invalidshuffle"),
+	)
+	require.Error(t, err)
+
+	// Invalid shuffle type
+	_, err = blosc.NewContext(
+		blosc.WithShuffle(blosc.ShuffleType(100)),
+	)
+	require.Error(t, err)
+
+	// Invalid compressor
+	_, err = blosc.NewContext(
+		blosc.WithCompressor("invalidcompressor"),
+	)
+	require.Error(t, err)
+}
+
+func TestShuffleTypes(t *testing.T) {
+	shuffles := blosc.ValidShuffles()
+	require.Equal(t, 3, len(shuffles))
+	require.Contains(t, shuffles, blosc.NoShuffle)
+	require.Contains(t, shuffles, blosc.ByteShuffle)
+	require.Contains(t, shuffles, blosc.BitShuffle)
+
+	// test string conversion
+	require.Equal(t, blosc.NoShuffleStr, blosc.NoShuffle.String())
+	require.Equal(t, blosc.ByteShuffleStr, blosc.ByteShuffle.String())
+	require.Equal(t, blosc.BitShuffleStr, blosc.BitShuffle.String())
+
+	// test FromString
+	var s blosc.ShuffleType
+	s.FromString("nosHUffle")
+	require.Equal(t, blosc.NoShuffle, s)
+	s.FromString("SHUffle")
+	require.Equal(t, blosc.ByteShuffle, s)
+	s.FromString("bitSHUffle")
+	require.Equal(t, blosc.BitShuffle, s)
+
+	s = blosc.ShuffleType(101)
+	require.Equal(t, "invalid", s.String())
+}
+
+func TestRoundTripDefaults(t *testing.T) {
+
+	ints := make([]int64, 10000)
+	for i := 0; i < len(ints); i++ {
+		ints[i] = int64(112)
+	}
+
+	// convert to byte slice
+	buf := &bytes.Buffer{}
+	err := binary.Write(buf, binary.LittleEndian, ints)
+	require.NoError(t, err)
+
+	c, err := blosc.NewContext()
+	require.NoError(t, err)
+
+	cmp, err := c.Compress(buf.Bytes())
+	require.NoError(t, err)
+	dec := c.Decompress(cmp)
+	require.Equal(t, len(buf.Bytes()), len(dec))
 }
 
 func TestEmpty(t *testing.T) {
 	var b []byte
-	err := blosc.SetCompressor("lz4")
+	c, err := blosc.NewContext()
 	require.NoError(t, err)
-
-	cmp, err := blosc.Compress(1, true, b)
+	cmp, err := c.Compress(b)
 	require.NoError(t, err)
-	dec := blosc.Decompress(cmp)
+	dec := c.Decompress(cmp)
 	require.Equal(t, 0, len(dec))
 }
